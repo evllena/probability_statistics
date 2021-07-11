49a093a4dfbf00058290dc9bf7e9776cec0aeaa2
evllena <helen.ermolaeva7@gmail.com>
2021-07-11 20:51:31 +1100

    less7 completed

--- hw7.py
+++ hw7.py
@@ -2,18 +2,24 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-
 zp1 = np.array([35, 45, 190, 200, 40, 70, 54, 150, 120, 110])
 zp = np.array([35, 45, 190, 200, 40, 70, 54, 150, 120, 110]).reshape(-1, 1)
 ks = np.array([401, 574, 874, 919, 459, 739, 653, 902, 746, 832])
 
-# Решение без interception
+# Решение без interception вариант 1
+
+b = (np.mean(zp1 * ks) - np.mean(zp1) * np.mean(ks)) / (np.mean(zp1**2) - np.mean(zp1) ** 2)
+a = np.mean(ks) - b * np.mean(zp1)
+print(f'b0 - {a}')
+print(f'b1 - {b}')
+
+# Решение без interception вариант 2
 
 n = 10
 ex = sum(zp1)
 ey = sum(ks)
-ex2 = sum(zp1**2)
-eyx = sum(zp1*ks)
+ex2 = sum(zp1 ** 2)
+eyx = sum(zp1 * ks)
 
 M1 = np.array([[n, ex], [ex, ex2]])
 v1 = np.array([ey, eyx])
@@ -37,7 +43,6 @@ print(f'slope (b1) - {coeff[0]}')
 x_new = np.arange(200).reshape(-1, 1)
 y_pred = model.predict(zp)
 y_new = model.predict(x_new)
-
 
 plt.plot(zp, ks, 'ro')
 plt.grid(True)
